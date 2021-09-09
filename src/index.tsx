99a558a3049d110dd534ef4b462c22654529b174
wsgcpd <90228134+wsgcpd@users.noreply.github.com>
2021-09-09 19:19:03 +0800

    Delete index.tsx

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { FramedDocumentRenderer } from "@govtechsg/decentralized-renderer-react-components";
-import { registry } from "./templates";
-
-ReactDOM.render(<FramedDocumentRenderer templateRegistry={registry} />, document.getElementById("root"));
