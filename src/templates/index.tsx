50ef125e6da8f289a69d89530cf06f5c4c8c410c
wsgcpd <90228134+wsgcpd@users.noreply.github.com>
2021-09-09 17:56:42 +0800

    Add files via upload

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,6 +1,6 @@
 import { TemplateRegistry } from "@govtechsg/decentralized-renderer-react-components";
-import { templates } from "./coc";
+import { templates } from "./cdf";
 
 export const registry: TemplateRegistry<any> = {
-  CDF: templates
+  COC: templates
 };
