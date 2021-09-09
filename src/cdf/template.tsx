f59c14f87cf5d04de9d146e0c81dab896883191f
wsgcpd <90228134+wsgcpd@users.noreply.github.com>
2021-09-09 16:02:57 +0800

    Add files via upload

--- src/cdf/template.tsx
+++ src/cdf/template.tsx
@@ -4,21 +4,28 @@ import { css } from "@emotion/core";
 import { CocTemplateCertificate } from "../samples/customTemplateSample";
 
 const containerStyle = css`
-  background-image: url(https://merriam-webster.com/assets/mw/images/article/art-wap-article-main/gray-background-7131-98db5e6ffc4972baa4087760291cbc3e@2x.jpg), 
-	url(https://i.pinimg.com/564x/88/b3/f6/88b3f63faa8791bf277126fcf307d691.jpg);
-  background-repeat: no-repeat,
+  background-image: url(https://github.com/wsgcpd/renderer/blob/master/src/cdf/images/background.png?raw=true);
+  background-size: cover;
   border: #340171;
   border-style: solid; 
-  box-sizing: border-box;
-  border-width: thick;
+  border-width: 10px;
   color: #340171;
   padding: 15px;
   margin: auto;
   width: 80%;
   text-align: center;
+
 p.a {
   font-family: "Lato";
   font-size: 12px;  
+  margin-bottom:0;
+} 
+
+p.b {
+  font-family: "Lato";
+  font-size: 12px;  
+  margin : 0; 
+  padding-top:0;
 } 
 
 p.name {
@@ -32,16 +39,17 @@ p.cred {
 }
 
 p.cename {
-  margin:0;
-  padding:0;
   font-family: "Libre Baskerville";
   font-size: 10px;
   font-weight: bold;
+  margin-bottom:0;
 }
 
 p.ce {
   font-family: "Lato";
   font-size: 9px;
+  margin : 0; 
+  padding-top:0;
 }
 
 `;
@@ -51,24 +59,27 @@ export const CocTemplate: FunctionComponent<
 > = ({ document, className = "" }) => {
   return (
     <div css={containerStyle} className={className} id="custom-template">
-	<img src="https://www.wsg.gov.sg/images/content/dam/ssg-wsg/header/wsg-header.png" />
+	<img src="https://github.com/wsgcpd/renderer/blob/master/src/cdf/images/wsglogo.png?raw=true" width="20%" />
 	<p class="a">THIS CERTIFIES THAT</p>
 	<p class="name"> {document.recipient.name}</p>
         <p class="a">has met all the requirements to be conferred the</p>
         <p class="cred">{document.name}</p>
         <p class="a">credential under the WSG Career Development Framework</p>
         <br />
-        <img src="https://www.wsg.gov.sg/images/content/dam/ssg-wsg/header/wsg-header.png" />
+        <img src="https://www.pngkey.com/png/full/799-7998866_eform-designer-just-drag-a-control-to-the.png" width="10%" />
         <hr width="220px" color="#340171" />
         <p class="cename">MR TAN CHOON SHIAN</p>
         <p class="ce">CHIEF EXECUTIVE</p>
         <p class="ce">WORKFORCE SINGAPORE</p>
+        <br />
 <table>
   <tr>
-    <td width="600px"><img src="https://www.wsg.gov.sg/images/content/dam/ssg-wsg/header/wsg-header.png" /></td>
-    <td><p class="a" align="left">Ref No.: {document.refno}</p> <p class="a" align="left">Certification Date: {document.certdate}</p> <p class="a" align="left">Recertify by: {document.recertifydate}</p></td>
+    <td width="600px"><img src="https://github.com/wsgcpd/renderer/blob/master/src/cdf/images/cdf.png?raw=true" width="70%" /></td>
+    <td><p class="a" align="left">Ref No.: {document.refno}</p> <p class="b" align="left">Certification Date: {document.certdate}</p> <p class="b" align="left">Recertify by: {document.recertifydate}</p></td>
   </tr>
 </table>
+<br />
+<br />
     </div>
   );
 };
